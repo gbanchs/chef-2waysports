e1f70dbc41b931414a90f03ca5a43484d62aae5a
Gabriel Banchs <gabanchsra31@hotmail.com>
2017-10-13 07:00:41 -0400

    Adding laravel opsworks

---
+++ laravel/recipes/storage_permissions.rb
@@ -0,0 +1,10 @@
+node[:deploy].each do |application, deploy|
+  script "storage_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    chmod -R 777 storage/app storage/framework storage/logs bootstrap/cache
+    EOH
+  end
+end
