b4235b2e58eab986e87d1f79322cf19fa7804a4a
gbanchs <gabanchsra31@hotmail.com>
2017-10-16 00:41:58 -0400

    Removing extra code

--- mysql/recipes/php_install.rb
+++ mysql/recipes/php_install.rb
@@ -4,13 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-
- bash 'install_php' do
-   user 'root'
-   cwd '/tmp'
-   code <<-EOH
- ["php", "libapache2-mod-php", "php-mcrypt", "php-mysql"].each do |pkg_name|
+["php", "libapache2-mod-php", "php-mcrypt", "php-mysql"].each do |pkg_name|
  package pkg_name
- end
-   EOH
  end
