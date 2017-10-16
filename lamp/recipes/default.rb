1da73d6166f012c25680f6b8ce75fb476a81f40c
gbanchs <gabanchsra31@hotmail.com>
2017-10-15 20:48:24 -0400

    Fixing apt_pakage command

--- lamp/recipes/default.rb
+++ lamp/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-package 'apache2' do
+apt_package 'apache2' do
   package_name node['apache']['package']
   default_release node['apache']['default_release'] unless node['apache']['default_release'].nil?
 end
