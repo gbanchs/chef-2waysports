ea23347b833fe57bb9cd1347b22f002ee74d11b0
gbanchs <gabanchsra31@hotmail.com>
2017-10-16 00:30:37 -0400

    Adding updates system from recipe

---
+++ updates/README.md
@@ -0,0 +1,4 @@
+# updates
+
+TODO: Enter the cookbook description here.
+
---
+++ updates/recipes/update.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: updates
+# Recipe:: update
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end
+
+package "update-notifier-common" do
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
+end
+
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+   File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+   File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
