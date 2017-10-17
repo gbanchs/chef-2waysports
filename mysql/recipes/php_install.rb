2a3c1e84b33f729c9a8f925312aff7e332987208
gbanchs <gabanchsra31@hotmail.com>
2017-10-17 19:49:47 -0400

    Adding apt update recipe to php install

--- mysql/recipes/php_install.rb
+++ mysql/recipes/php_install.rb
@@ -4,6 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-["php", "libapache2-mod-php", "php-mcrypt", "php-mysql"].each do |pkg_name|
+include_recipe "apt"
+
+["php", "libapache2-mod-php", "php-mcrypt", "php-mysql", "php7.0-mbstring", "phpunit", "zip", "unzip", "php7.0-zip"].each do |pkg_name|
  package pkg_name
  end
+
+
+#must enabled sudo a2enmod rewrite
