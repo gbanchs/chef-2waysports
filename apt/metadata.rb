b0ef49cb2c9e55c5266f151cf3217b4a4f5617d9
gbanchs <gabanchsra31@hotmail.com>
2017-10-18 07:49:03 -0400

    Adding php applications dependencies

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -28,3 +28,7 @@ attribute "apt/key_proxy",
 attribute "apt/caching_server",
   :description => "Set this to true if the node is a caching server",
   :default => "false"
+
+
+depends 'application'
+depends 'application_php'
